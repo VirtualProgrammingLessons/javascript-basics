dbb253d3c95331eb7f7395f0fe55c37535c9d31c
ErinCleaver-GRCC <77419665+ErinCleaver-GRCC@users.noreply.github.com>
2022-06-09 08:02:03 -0400

    Update

---
+++ Lesson 2 - doing math/javascript.js
@@ -0,0 +1,31 @@
+// + is used to do addition.
+
+
+
+// - is use to do subtraction.
+
+
+
+//  * is used to do multiplication.
+
+
+
+// / is used to do division.
+
+
+
+// ** Is used to find the power of a number.
+
+
+// % Is used to find the remainder (Modules)
+
+
+// ++ Increments the value by a number.
+
+
+
+// -- Decrements the value by a number.
+
+
+
+
