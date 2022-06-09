62f46d08855053fb44a5ee57cf5a177a1d6a291f
ErinCleaver-GRCC <77419665+ErinCleaver-GRCC@users.noreply.github.com>
2022-06-09 06:21:43 -0400

    update

---
+++ Lesson 1 - variables/javascript.js
@@ -0,0 +1,17 @@
+/*
+Their are three types of variables in javascript that
+var, let, and const
+var
+  *  Is gobal scope
+  * can only be declared once
+let
+  *  let is a local variable and is the perfered way to declare variables
+  *  Can only declared once in
+const
+  * Cannot be changed
+  *  Is local in scope
+*/
+
+
+
+
