cef00d86f83b748bcede2998fb35f70a00d5e4a9
ErinCleaver-GRCC <77419665+ErinCleaver-GRCC@users.noreply.github.com>
2022-06-13 08:05:02 -0400

    Update javascript.js
    update

--- Lesson 2 - doing math/javascript.js
+++ Lesson 2 - doing math/javascript.js
@@ -9,24 +9,48 @@ For learning basic programming without downloading software to follow the lesson
 */
 
 // + is used to do addition.
+let x = 5
+let y = 10
+let add = x + y
 
+console.log(`${x} + ${y} is: ${add}`)
+console.log(x + " + " + y + " is: " + add)
 
 // - is use to do subtraction.
+let subtraction = y - x
+console.log(`${y} - ${x} is: ${subtraction}`)
 
 
 //  * is used to do multiplication.
+let multiplication = x * y
+console.log(`${x} * ${y} = ${multiplication}`)
 
 // / is used to do division.
+let division = y / x
+console.log(`${y} / ${x} = ${division}`)
 
 
 // ** Is used to find the power of a number.
+let power = x ** 2
+console.log(`${x} ** 2 is: ${power}`)
+
 
 // % Is used to find the remainder (Modules)
+let reminder = y % 3
+console.log(`${y} % 3 is: ${reminder}`)
+
 
 // ++ Increments the value by a number.
+let count = 1;
+count++;
+count++;
+
+console.log(count)
 
 // -- Decrements the value by a number.
-
+count--
+count--
+console.log(count)
 
 
 
