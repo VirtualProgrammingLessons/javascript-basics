ec129ac372a558dcd750131df3cf5257dae4f1bc
ErinCleaver-GRCC <77419665+ErinCleaver-GRCC@users.noreply.github.com>
2022-06-25 09:28:46 -0400

    update

---
+++ Lesson 3 - IF_Else_ And_IF_Else/javascript.js
@@ -0,0 +1,17 @@
+/*
+
+For learning basic programming without downloading software to follow the lesson:
+
+*  Replit:  https://replit.com/
+*  CodeSandbox:  https://codesandbox.io/
+*  Stackblitz:  https://stackblitz.com/
+
+*/
+
+// if statements
+
+
+// else statements
+
+
+// else if statements
